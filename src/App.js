51aad5836e1a50f466a4d408ff0d840abdeb3c16
mikolajfilipkowski <mikolajfilipkowski000@gmail.com>
2022-12-16 09:40:02 +0100

    Modify counter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import Counter from './components/Counter.jsx'
 
 function App() {
   return (
+    <>
+    <Counter count={2137}/>
+    <Counter count={420}/>
+    <Counter count={69696969}/>
     <Counter/>
+    </>
   );
 }
 
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: props.count ? props.count : 0,
+        }
+    }
+    formatCount(){
+        const {count} = this.state;
+        return count === 0 ? "Zero" : count;
+    }
+
     render(){
         return(
-        <>Siema!</>
+        <>
+            <span className='badge badge-primary m-1 p-2'>{this.formatCount(this.state.count)}</span>
+            <button onClick={() => this.setState({count:this.state.count+1})} className='btn btn-success btn-sm m-1'>Increment</button>
+            <button onClick={() => this.setState({count:this.state.count-1})} className='btn btn-danger btn-sm m-1'>Decrement</button>
+        </>
         )
     }
 }
